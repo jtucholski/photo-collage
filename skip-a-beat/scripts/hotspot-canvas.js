0f81c7c23e6ceea55a0c27d406f7749cb42854fa
Josh Tucholski <josh@techelevator.com>
2017-09-08 23:22:36 -0400

    Made hotspot clickable.

--- skip-a-beat/scripts/hotspot-canvas.js
+++ skip-a-beat/scripts/hotspot-canvas.js
@@ -1,23 +1,18 @@
-HotspotCanvas = function (hotspots, canvas_id, backgroundImage) {
+HotspotCanvas = function (hotspots, canvas_id, backgroundImage, eventCallback) {
 
     this.canvas = new fabric.Canvas(canvas_id);
+
 
     this.setBackground(backgroundImage);
     this.setHotspots(hotspots);
     this.setEventHandlers();
+    
 
-    this.selectedPhotoUrl = null;
+    this.eventCallback = eventCallback;
 }
 
 HotspotCanvas.prototype = {
 
-    getSelectedPhotoUrl: function () {
-        return this.selectedPhotoUrl;
-    },
-
-    setSelectedPhotoUrl: function (url) {
-        this.selectedPhotoUrl = url;
-    },
 
     setBackground: function (backgroundImage) {
         this.addImage(backgroundImage, { selectable: false }, function (img) {
@@ -25,51 +20,124 @@ HotspotCanvas.prototype = {
         });
     },
 
+    addImageToHotspot: function (hotspotRect, url) {
+        var _this = this;
+        
+        hotspotRect.image = true;        
+
+        this.addImage(url,
+            {
+                originX: 'left',
+                originY: 'top',
+                lockUniScaling: true,
+                lockRotation: false,
+                lockScalingFlip: true,
+                left: hotspotRect.hotspot.x,
+                top: hotspotRect.hotspot.y,
+                clipTo: function (ctx) {
+                    var retina = _this.canvas.getRetinaScaling();
+                    ctx.save();
+                    ctx.setTransform(retina, 0, 0, retina, 0, 0);
+                    ctx.rect(
+                        hotspotRect.left,
+                        hotspotRect.top,
+                        hotspotRect.width,
+                        hotspotRect.height);
+                    ctx.restore();
+                },
+
+            }, function (img) {
+                                
+                if (hotspotRect.width > hotspotRect.height) {
+                    img.scaleToWidth(hotspotRect.width);
+                }
+                else {
+                    img.scaleToHeight(hotspotRect.height);
+                }
+            });
+    },
+
+    downloadCanvas: function () {        
+
+        var dataURLtoBlob = function (dataurl) {
+            var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+                bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+            while (n--) {
+                u8arr[n] = bstr.charCodeAt(n);
+            }
+            return new Blob([u8arr], { type: mime });
+        }
+
+        var link = document.createElement("a");
+        var imgData = this.canvas.toDataURL({
+            format: 'png',
+            multiplier: 4
+        });
+        var strDataURI = imgData.substr(22, imgData.length);
+        var blob = dataURLtoBlob(imgData);
+        var objurl = URL.createObjectURL(blob);
+
+        link.download = "helloWorld.png";
+
+        link.href = objurl;
+
+        link.click();
+
+    },
+
+    
+
     setEventHandlers: function () {
         var _this = this;
+
+        var disableScroll = function () {
+            _this.canvas.allowTouchScrolling = false;
+        };
+
+        var enableScroll = function () {
+            _this.canvas.allowTouchScrolling = true;
+        };
+
+        this.canvas.on('object:moving', disableScroll);
+        this.canvas.on('object:scaling', disableScroll);
+        this.canvas.on('object:rotating', disableScroll);
+        this.canvas.on('mouse:up', enableScroll);
+
+
         var objects = this.canvas.getObjects('rect');
         objects.forEach(function (rect) {
+            
             rect.on('mouseup', function () {
 
+                _this._rect = this;
+                var imageSelector = $("#imageselector");
+                imageSelector.click();               
+                
 
-                if (_this.enabled) {
-                    var _hotspotRect = this;
-
-                    _this.addImage(_this.selectedPhotoUrl,
-                        {
-                            originX: 'left',
-                            originY: 'top',
-                            lockUniScaling: true,
-                            lockRotation: true,
-                            left: _hotspotRect.hotspot.x,
-                            top: _hotspotRect.hotspot.y,
-                            clipTo: function (ctx) {
-                                var retina = _this.canvas.getRetinaScaling();
-                                ctx.save();
-                                ctx.setTransform(retina, 0, 0, retina, 0, 0);
-                                ctx.rect(
-                                    _hotspotRect.left,
-                                    _hotspotRect.top,
-                                    _hotspotRect.width,
-                                    _hotspotRect.height);
-                                ctx.restore();
-                            }
-                        }, function (img) {
-
-                            if (_hotspotRect.width > _hotspotRect.height) {
-                                img.scaleToWidth(_hotspotRect.width);
-                            }
-                            else {
-                                img.scaleToHeight(_hotspotRect.height);
-                            }
-
-                        });
-
-                    _this.enableHotspots(false);
-                }
             }, this);
         }, this);
 
+        $("#imageselector").on("change", function(e) { 
+            if (this.files) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    _this.addImageToHotspot(_this._rect, e.target.result);     
+                    _this.eventCallback({
+                        hotspotsRemaining: _this.getHotspotsWithoutImages(),
+                        hotspots: _this.canvas.getObjects('rect').length
+                    })                       
+                }
+                reader.readAsDataURL(this.files[0]);
+            }
+        });
+
+    },
+
+    getHotspotsWithoutImages: function () {
+        var rects = this.canvas.getObjects('rect');
+        return rects.filter((rect) => {
+            return rect.image === undefined || rect.image === false;
+        }).length;
     },
 
     setHotspots: function (hotspots) {
@@ -82,8 +150,6 @@ HotspotCanvas.prototype = {
                 top: hotspot.y,
                 height: hotspot.height,
                 width: hotspot.width,
-                // height: hotspot.height,
-                // width: hotspot.width,
                 fill: 'transparent',
                 stroke: '#000',
                 strokeWidth: 0,
@@ -103,39 +169,70 @@ HotspotCanvas.prototype = {
     */
     addImage: function (imageUrl, properties, callback) {
         var _this = this;
-        fabric.Image.fromURL(imageUrl, function (img) {
 
-            if (properties !== undefined) {
-                for (var p in properties) {
-                    img.set(p, properties[p]);
+        loadImage(imageUrl, function(img) { 
+            var image = new fabric.Image(img);
+
+            if (properties !== undefined) { 
+                for(var p in properties) { 
+                    image.set(p, properties[p]);
                 }
             }
 
-            _this.canvas.add(img);
+            _this.canvas.add(image);
 
-            if (callback !== undefined) {
-                callback(img);
-            }
-        });
+            if (callback !== undefined) { 
+                callback(image);
+            }    
+        }, { canvas: true});
+
+        // var imageObj = new Image();
+        
+        // imageObj.onload = function() { 
+
+            
+
+        //     var image = new fabric.Image(imageObj);
+
+        //     if (properties !== undefined) { 
+        //         for(var p in properties) { 
+        //             image.set(p, properties[p]);
+        //         }
+        //     }
+
+        //     _this.canvas.add(image);
+
+        //     if (callback !== undefined) { 
+        //         callback(image);
+        //     }
+        // }
+        // imageObj.crossOrigin = 'anonymous';
+        // imageObj.src = imageUrl;
+
+        // fabric.Image.fromURL(imageUrl, function (img) {
+
+        //     if (properties !== undefined) {
+        //         for (var p in properties) {
+        //             img.set(p, properties[p]);
+        //         }
+        //     }
+
+        //     _this.canvas.add(img);
+
+        //     if (callback !== undefined) {
+        //         callback(img);
+        //     }
+        // });
     },
 
     getDataUrl: function () {
-        return this.canvas.toDataURL();
+        return this.canvas.toDataURL({
+
+        });
     },
 
-    enableHotspots: function (shouldEnable) {
 
-        var strokeWidth = (shouldEnable) ? 1 : 0;
 
-        this.enabled = shouldEnable;
-
-        var objects = this.canvas.getObjects('rect');
-        objects.forEach(function (rect) {
-            rect.set('strokeWidth', strokeWidth);
-            rect.bringToFront();
-        }, this);
-
-    }
 
 }
 
